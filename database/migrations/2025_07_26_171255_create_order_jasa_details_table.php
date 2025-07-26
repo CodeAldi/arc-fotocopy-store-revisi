b550bb2038cdd0979f9924f52f88e3d36ce0cb88
CodeAldi <codealdi@gmail.com>
2025-07-26 17:21:36 +0700

    create models, migration, and controller for order jasa details

---
+++ database/migrations/2025_07_26_171255_create_order_jasa_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_jasa_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('order')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('jasa_id')->constrained('jasa')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('file');
+            $table->string('catatan')->nullable();
+            $table->integer('jumlah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_jasa_details');
+    }
+};
